a179a6de3b111899da002810198074df5923a2dd
GustiMuhammadNaufalRazin <115336686+GustiMuhammadNaufalRazin@users.noreply.github.com>
2023-09-17 16:03:33 +0800

    Add files via upload

---
+++ MODUL1/PRAK001_2210817210002_GustiMuhammadNaufalRazin.java
@@ -0,0 +1,7 @@
+public class PRAK001_2210817210002_GustiMuhammadNaufalRazin {
+    public static void main(String[] args) {
+        System.out.println("Selamat Pagi Gusti Muhammad Naufal Razin");
+        System.out.println("Selamat Siang Gusti Muhammad Naufal Razin");
+        System.out.println("Selamat Malam Gusti Muhammad Naufal Razin");
+    }
+}
---
+++ MODUL1/PRAK002_2210817210002_GustiMuhammadNaufalRazin.java
@@ -0,0 +1,10 @@
+public class PRAK002_2210817210002_GustiMuhammadNaufalRazin {
+    public static void main(String[] args) {
+        System.out.println("###################################");
+        System.out.println("#                                 #");
+        System.out.println("#   Gusti Muhammad Naufal Razin   #");
+        System.out.println("#         2210817210002           #");
+        System.out.println("#                                 #");
+        System.out.println("###################################");
+    }
+}
